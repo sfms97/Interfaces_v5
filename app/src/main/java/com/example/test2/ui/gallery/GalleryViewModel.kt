86f63a84f5afa3e1d6bed96412cc353ebae5f944
Samuel Fernando Méndez Saldías <feernandomemdez@gmail.com>
2022-06-01 17:26:10 +0200

    Vistas

--- app/src/main/java/com/example/test2/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/com/example/test2/ui/gallery/GalleryViewModel.kt
@@ -9,10 +9,11 @@ class GalleryViewModel : ViewModel() {
     private val _text = MutableLiveData<String>().apply {
         value = "Ocio cercano"
     }
-    private val _text2 = MutableLiveData<String>().apply {
-        value = "Ocio cercano"
-    }
+
     val text: LiveData<String> = _text
 
+    private val _text2 = MutableLiveData<String>().apply {
+        value = "Buscar"
+    }
     val text2: LiveData<String> = _text2
 }
