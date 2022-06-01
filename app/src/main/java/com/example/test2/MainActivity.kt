c6853514010ff57a2a7c1aa9336a9bdc2123a2d2
Alejandro Fernández Gete <22171935@live.uem.es>
2022-06-01 17:29:54 +0200

    Imagenes, colores y fragments

--- app/src/main/java/com/example/test2/MainActivity.kt
+++ app/src/main/java/com/example/test2/MainActivity.kt
@@ -37,7 +37,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.blankFragment
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.ApuntesFragment, R.id.PerfilFragment
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
