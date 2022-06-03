f40d073a7d9799bd8171f684d40f0b6dbe5d1bcb
Alejandro Fernández Gete <22171935@live.uem.es>
2022-06-03 21:57:11 +0200

    Intento de mapas y log out

--- app/src/main/java/com/example/test2/MainActivity.kt
+++ app/src/main/java/com/example/test2/MainActivity.kt
@@ -54,7 +54,7 @@ class MainActivity : AppCompatActivity() , OnMapReadyCallback {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.blankFragment, R.id.PerfilFragment
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.blankFragment, R.id.PerfilFragment, R.id.nav_logout
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
---
+++ app/src/main/java/com/example/test2/MapFragment.kt
@@ -0,0 +1,50 @@
+package com.example.test2
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.test2.R
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.CameraUpdateFactory
+
+class MapFragment : Fragment() {
+    /*override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Initialize view
+        val view: View = inflater.inflate(R.layout.fragment_map, container, false)
+
+        // Initialize map fragment
+        val supportMapFragment =
+            childFragmentManager.findFragmentById(R.id.google_map) as SupportMapFragment?
+
+        // Async map
+        supportMapFragment!!.getMapAsync { googleMap ->
+            // When map is loaded
+            googleMap.setOnMapClickListener { latLng -> // When clicked on map
+                // Initialize marker options
+                val markerOptions = MarkerOptions()
+                // Set position of marker
+                markerOptions.position(latLng)
+                // Set title of marker
+                markerOptions.title(latLng.latitude.toString() + " : " + latLng.longitude)
+                // Remove all marker
+                googleMap.clear()
+                // Animating to zoom the marker
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10f))
+                // Add marker on map
+                googleMap.addMarker(markerOptions)
+            }
+        }
+        // Return view
+        return view
+    }*/
+}
