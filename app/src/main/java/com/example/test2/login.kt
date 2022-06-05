a1fae479eedf4481f58c857cc5f234d023535d6d
Samuel Fernando Méndez Saldías <feernandomemdez@gmail.com>
2022-06-05 18:09:06 +0200

    Registro

--- app/src/main/java/com/example/test2/login.kt
+++ app/src/main/java/com/example/test2/login.kt
@@ -27,16 +27,6 @@ class login : AppCompatActivity() {
         username = findViewById<View>(R.id.editTextTextPersonName) as EditText
         password = findViewById<View>(R.id.editTextTextPassword) as EditText
         var boton: TextView = findViewById(R.id.txtLogin)
-       /* var go: Button = findViewById(R.id.btnGo)
-
-        go.setOnClickListener {
-
-            val intent = Intent(this, goPressed()::class.java)
-            val intent2 = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            startActivity(intent2)
-        }*/
-
         boton.setOnClickListener {
             val userData: MutableList<User> = ArrayList()
             var reader: BufferedReader? = null
@@ -99,55 +89,6 @@ class login : AppCompatActivity() {
     }
 
 
-    @Throws(IOException::class)
-    fun goPressed() {
-        val userData: MutableList<User> = ArrayList()
-        var reader: BufferedReader? = null
-
-        println("HOLA")
-
-        try {
-            getUsername1 = username!!.text.toString()
-            getPassword1 = password!!.text.toString()
-            println("entra en try")
-            val fileName = "loginDetails.csv"
-
-            reader = BufferedReader(InputStreamReader(assets.open(fileName), "UTF-8"))
-            reader.readLine()
-            println("HOLA2")
-            var line = ""
-            while (reader.readLine().also { line = it } != null) {
-                println("si o que")
-                val token = line.split(",").toTypedArray()
-                if (token.size > 0) {
-                    val data = User()
-                    data.setId(token[0].toInt())
-                    data.setUsername(token[1])
-                    data.setPassword(token[2])
-                    userData.add(data)
-                    Log.d("Activity", "" + data)
-                    println(data)
-                    println("HHHHHHHOLA")
-                    var i: Int
-                    i = 0
-                    while (i < userData.size) {
-                        if (data.username == getUsername1 && data.password == getPassword1) {
-                            println("Valores correctos")
-                            Toast.makeText(
-                                applicationContext,
-                                "Password and Username is Correct",
-                                Toast.LENGTH_SHORT
-                            ).show()
-                        }
-                        i++
-                    }
-                }
-            }
-        } catch (e: IOException) {
-            e.printStackTrace()
-            //println("HHHHHHHOLA")
-        }
-    }
 
 
 }
--- app/src/main/java/com/example/test2/registrate.kt
+++ app/src/main/java/com/example/test2/registrate.kt
@@ -1,20 +1,72 @@
 package com.example.test2
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.io.BufferedWriter
+import java.io.File
+import java.io.FileWriter
 
 class registrate : AppCompatActivity() {
+
+    var usernameRegistro: EditText? = null
+    var passwordRegistro: EditText? = null
+    var nombreRegistro: EditText? = null
+    var apellidoRegistro: EditText? = null
+    var emailRegistro: EditText? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registrate)
 
+        usernameRegistro = findViewById(R.id.editTextTextName) as EditText
+        passwordRegistro = findViewById(R.id.editTextTextLastname) as EditText
+        nombreRegistro = findViewById(R.id.editTextTextEmail) as EditText
+        apellidoRegistro = findViewById(R.id.editTextTextPersonName) as EditText
+        emailRegistro = findViewById(R.id.editTextTextPassword) as EditText
+
+        var getUsernameR: String? = null
+        var getPasswordR: String? = null
+        var getNombreR: String? = null
+        var getApellidoR: String? = null
+        var getEmailR: String? = null
+
+
+
         var botonRegistro : TextView = findViewById(R.id.txtSignUp)
 
         botonRegistro.setOnClickListener{
-            val intent = Intent(this, login::class.java)
-            startActivity(intent)
+            getUsernameR = usernameRegistro!!.text.toString()
+            getPasswordR = passwordRegistro!!.text.toString()
+            getNombreR = nombreRegistro!!.text.toString()
+            getApellidoR = apellidoRegistro!!.text.toString()
+            getEmailR = emailRegistro!!.text.toString()
+
+            println("HOOOOOOOOOOOOOOOLA")
+            val fileName = "app/base.apk/assets/loginDetails.csv"
+            //val myFile = File(fileName)
+
+            val contenido = getUsernameR +"," + getPasswordR + "," + getNombreR + "," + getApellidoR +","+ getEmailR
+
+
+            try{
+                val file = File(fileName)
+
+                val fw = FileWriter(fileName)
+                val bw = BufferedWriter(fw)
+                bw.write(contenido)
+                bw.close()
+
+                println(contenido)
+
+                val intent = Intent(this, login::class.java)
+                startActivity(intent)
+
+            }catch (e: java.io.FileNotFoundException){
+                println("NOT FOUNDDDDDDDDDDDDDDD")
+            }
         }
     }
 }
